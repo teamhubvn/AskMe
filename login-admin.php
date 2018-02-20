307eb3a48d64468df8879203f3f9248f9f55eab9
teamhubvn <36300459+teamhubvn@users.noreply.github.com>
2018-02-20 19:35:50 +0700

    Create login-admin.php

---
+++ login-admin.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans|Rancho" rel="stylesheet">
+  <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/animate.css@3.5.2/animate.min.css">
+  <link rel="stylesheet" href="login-admin.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/animate.css@3.5.2/animate.min.css" integrity="sha384-OHBBOqpYHNsIqQy8hL1U+8OXf9hH6QRxi0+EODezv82DfnZoV7qoHAZDwMwEJvSw" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <link href='https://pewrank.blogspot.com/favicon.ico' rel='icon' type='image/x-icon'/>
+  <meta content='100012668051362' property='fb:admins'/>
+  <meta content='author' name='Sven'/>
+<title>Login</title>
+</head>
+<div id="main" class="animated twice fadeInDown">
+	<h3><span class="color-1">My</span><span class="color-2"> inbox</span>
+</h3>
+<center><img alt="image" src="https://image.flaticon.com/icons/svg/134/134908.svg" width="125px" height="125px"></center>
+<form action="admlogin.php" method="post">
+<input type="text" name="username" id="textControl" placeholder="" class="user-icon">
+<input type="password" name="password" id="passControl" placeholder="" class="pass-icon">
+ <button styp="submit" name="log" id="loginControl">
+ Login
+ </button>
+</form>
+ <a href="" id="regist">Regist</a>
+</div>
+</body>
+</html>
+
